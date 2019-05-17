3d0dbafecd7a8ad47e5fe8b76ba82421cc662d95
Arthur Torres <arthurtorres002@gmail.com>
2019-05-17 18:32:16 -0400

    one_away.rb complexity

--- Ruby/one_away.rb
+++ Ruby/one_away.rb
@@ -16,14 +16,18 @@ first3 = 'pale'
 second3 = 'bake'
 # false
 
+# O(3N) = O(N)
 def one_away?(first, second)
+  # O(1)
   strings = [first, second].sort_by { |string| string.length}
+  # O(2N) = O(N)
   strings = strings.map { |string| string.split('') }
   action_taken = false
   dif = strings[1].count - strings[0].count
 
   return false if  dif < 0 || dif > 1
 
+  # O(N)
   strings[1].each_with_index do |base, index|
     index = index - 1 if action_taken && dif == 1
 
