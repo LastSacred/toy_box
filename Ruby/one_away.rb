be81a9dcfe582c6109947fd081b8d3d28751af00
Arthur Torres <arthurtorres002@gmail.com>
2019-05-17 18:25:50 -0400

    one_away

---
+++ Ruby/one_away.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+first0 = 'pale'
+second0 = 'ple'
+# true
+
+first1 = 'pales'
+second1 = 'pale'
+# true
+
+first2 = 'pale'
+second2 = 'bale'
+# true
+
+first3 = 'pale'
+second3 = 'bake'
+# false
+
+def one_away?(first, second)
+  strings = [first, second].sort_by { |string| string.length}
+  strings = strings.map { |string| string.split('') }
+  action_taken = false
+  dif = strings[1].count - strings[0].count
+
+  return false if  dif < 0 || dif > 1
+
+  strings[1].each_with_index do |base, index|
+    index = index - 1 if action_taken && dif == 1
+
+    if base != strings[0][index]
+      return false if action_taken
+      action_taken = true
+    end
+  end
+
+  true
+end
+
+puts one_away?(first0, second0)
+puts one_away?(first1, second1)
+puts one_away?(first2, second2)
+puts one_away?(first3, second3)
