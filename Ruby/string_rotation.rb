8a6e10dcb0e627bf1231e46e89b8098aa076c6c4
Arthur Torres <arthurtorres002@gmail.com>
2019-05-18 14:54:32 -0400

    string_rotation.rb

--- Ruby/string_rotation.rb
+++ Ruby/string_rotation.rb
@@ -4,8 +4,22 @@ s1 = 'waterbottle'
 s2 = 'erbottlewat'
 #  true
 
-def is_rotation(s1, s2)
-  s1.length.times do
-    binding.pry
+# O(N)
+def is_rotation_a(s1, s2)
+  s1 = s1.split('')
+  s2 = s2.split('')
+
+  (s1.length).times do
+    s1 << s1.shift
+    return true if s1 == s2
   end
+
+  false
 end
+
+# O(N)
+def is_rotation_b(s1, s2)
+  (s1 * 2).include?(s2)
+end
+
+puts is_rotation_b(s1, s2)
